ea3c1e87f22b49f0e458ce9492cac823c3d605a0
Connor Matheny <connormatheny1@gmail.com>
2021-06-08 18:07:35 -0500

    turn on ssl

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: false })
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: true })
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
